2e6a71412da71fdd602342086d6a6ab5a47af901
Sergio Magariño <sergio.magarino@clouddistrict.com>
2017-04-25 12:28:40 +0200

    Implement tutorial

---
+++ src/commons/Colors.js
@@ -0,0 +1,30 @@
+export const black = '#222222'
+export const black_light = '#333333'
+
+export const white = 'white'
+export const white_back = '#F9F9F9'
+
+export const gray_border = '#E4E4E4'
+export const gray_placeholder = '#B2B2B2'
+export const gray_info = '#3E4A4B'
+export const gray_date = '#8A8A8A'
+export const gray_menu = '#666666'
+
+export const green = '#1FA527'
+export const green_overlay = 'rgba(31,165,39, 0.9)'
+export const green_dark = '#118E87'
+export const green_light = '#01CD82'
+
+export const yellow = '#FFC111'
+export const gold = '#DD9E1E'
+
+export const red = '#D75644'
+export const red_delete = '#E23E57'
+
+export const orange = '#F19327'
+export const tile = '#f47542'
+
+export const blue = '#05A8C4'
+export const blue_main = '#1F4BA5'
+export const blue_dark = '#3B5998'
+export const blue_light = '#55ACEE'
