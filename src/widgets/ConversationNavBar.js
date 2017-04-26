1cbfdebeeb726dc30c3a4e441fc594bdc4c7d5e4
Sergio Magariño <sergio.magarino@clouddistrict.com>
2017-04-26 14:58:46 +0200

    Hide edit button in chat scene

--- src/widgets/ConversationNavBar.js
+++ src/widgets/ConversationNavBar.js
@@ -23,13 +23,15 @@ class ConversationNavBar extends Component {
           <Image source={require('keynos_app/src/resources/arrowback.png')} style={{height: 22, width: 18, tintColor: main_color}} resizeMode={'contain'} />
         </TouchableOpacity>
         <Image source={{uri: data.bot_image}} style={{height: 58, width: 54, marginHorizontal: 10, borderRadius: 10}} resizeMode={'cover'} />
-        <View style={{flex: 1}}>
+        <View style={{flex: 1, marginRight: 10}}>
           <Text style={{color: Colors.gray_info, fontSize: 17}} numberOfLines={1}>{data.interlocutor}</Text>
           <Text style={{color: Colors.gray_info, fontSize: 15, marginVertical: 8}} numberOfLines={1}>{data.conversation_title}</Text>
         </View>
-        <TouchableOpacity style={{marginHorizontal: 10}}>
+        {/*
+        <TouchableOpacity style={{marginRight: 10}}>
           <Text style={{color: main_color, fontSize: 15}}>{multiStrings.edit}</Text>
         </TouchableOpacity>
+        */}
       </View>
     )
   }
