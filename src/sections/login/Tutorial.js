f961069155440d2c49fdeee10756406c29d2c17a
Sergio Magariño <sergio.magarino@clouddistrict.com>
2017-04-27 17:49:32 +0200

    Hide skip tutorial button for first time running app

--- src/sections/login/Tutorial.js
+++ src/sections/login/Tutorial.js
@@ -39,29 +39,37 @@ class Tutorial extends Component {
     }
   }
 
+  renderSkipButton() {
+    if(this.state.enableSkip) {
+      return(
+        <TouchableOpacity style={{backgroundColor: Colors.green_light, alignItems: 'center', justifyContent: 'center', borderRadius: 3, padding: 12, margin: 20}}
+          onPress={() => Actions.CompanySelection()}>
+          <Text style={{color: Colors.white, fontSize: 17}}>{multiStrings.accessToCompany}</Text>
+        </TouchableOpacity>
+      )
+    }
+  }
+
   render() {
     return (
       <View style={{flex: 1}} >
-          <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 80, marginBottom: 50}} >
-            <Text style={{color: Colors.green_light, fontSize: 20, marginBottom: 5}} >{multiStrings.tutorialTitle}</Text>
-            <Text style={{color: Colors.gray_info, fontSize: 17, textAlign: 'center'}} >{multiStrings.tutorialSubtitle}</Text>
+        <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 80, marginBottom: 50}} >
+          <Text style={{color: Colors.green_light, fontSize: 20, marginBottom: 5}} >{multiStrings.tutorialTitle}</Text>
+          <Text style={{color: Colors.gray_info, fontSize: 17, textAlign: 'center'}} >{multiStrings.tutorialSubtitle}</Text>
+        </View>
+        <Swiper height={240} dotColor={Colors.green_light} activeDotColor={Colors.green_active}
+          onMomentumScrollEnd={(e, state, context) => this.enableSkipTutorial(state.index)} >
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}} >
+            <Text style={{color: Colors.black, fontSize: 20, marginBottom: 5}} >{'tutorial 1'}</Text>
           </View>
-          <Swiper height={240} dotColor={Colors.green_light} activeDotColor={Colors.green_active}
-            onMomentumScrollEnd={(e, state, context) => this.enableSkipTutorial(state.index)} >
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}} >
-              <Text style={{color: Colors.black, fontSize: 20, marginBottom: 5}} >{'tutorial 1'}</Text>
-            </View>
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}} >
-              <Text style={{color: Colors.black, fontSize: 20, marginBottom: 5}} >{'tutorial 2'}</Text>
-            </View>
-          </Swiper>
-          <TouchableOpacity disabled={!this.state.enableSkip} style={{backgroundColor: Colors.green_light, alignItems: 'center', justifyContent: 'center', borderRadius: 3, padding: 12, margin: 20}}
-    				onPress={() => Actions.CompanySelection()}>
-    				<Text style={{color: Colors.white, fontSize: 17}}>{multiStrings.accessToCompany}</Text>
-          </TouchableOpacity>
-          <View style={{alignItems: 'center', justifyContent: 'center', margin: 20}} >
-            <Image source={require('keynos_app/src/resources/logo.png')} style={{height: 30, width: 136}} resizeMode={'contain'} />
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}} >
+            <Text style={{color: Colors.black, fontSize: 20, marginBottom: 5}} >{'tutorial 2'}</Text>
           </View>
+        </Swiper>
+        {this.renderSkipButton()}
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'flex-end', margin: 20, marginBottom: 60}} >
+          <Image source={require('keynos_app/src/resources/logo.png')} style={{height: 30, width: 136}} resizeMode={'contain'} />
+        </View>
       </View>
     );
   }
